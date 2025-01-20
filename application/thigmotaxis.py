- training with background normalization, but immediately abandoned - a few fixes of evaluations

application/thigmotaxis.py
@@ -62,7 +62,7 @@ data = pd.concat([calculate_speed(segment) for segment in selected_segments])
 
 ### Smoothness
 BV = abs_variation(data)
-P['mean_dz'] = BV
+P['mean_dz'] = float(BV)
 print('Mean temporal variation of z:', BV, 'um')
 
 ### Bimodal fit
@@ -73,9 +73,9 @@ gmm.fit(data['z'].values.reshape(-1, 1))
 means = gmm.means_.flatten()
 stds = np.sqrt(gmm.covariances_).flatten()
 dprime = np.abs(.5*(stds[0]+stds[1])/(means[1]-means[0]))
-P['mu1'], P['mu2'] = means
-P['std1'], P['std2'] = stds
-P['dprime'] = dprime
+P['mu1'], P['mu2'] = float(means[0]), float(means[1])
+P['std1'], P['std2'] = float(stds[0]), float(stds[1])
+P['dprime'] = float(dprime)
 
 print()
 print('Means:', means)

dataset/create_dataset.py
@@ -72,9 +72,8 @@ else:
 ### Background normalization
 if background_path:
     background = imageio.imread(background_path)
-    normalization = np.mean(background)
 else:
-    normalization = 1.
+    background = None
 
 ### Open movie
 if movie_filename.endswith('.zip'):
@@ -105,23 +104,31 @@ selected_segments = [segment for segment in traj if \
 data = pd.concat(selected_segments)
 
 j = 0
+intensities = []
 previous_position = 0
 for image in tqdm.tqdm(movie.frames(), total=n_frames):
     data_frame = data[data['frame'] == previous_position]
-    snippets = extract_cells(image, data_frame, image_size, crop=True, borders=False) # remove border cells because they give away x and therefore z
+    snippets = extract_cells(image, data_frame, image_size, crop=True)
+    if not background_path:
+        intensities.extend([np.mean(snippet) for snippet in snippets])
 
     i = 0 # this is snippet number
     for _, row in data_frame.iterrows():
-        if snippets[i] is not None:
-            j += 1 # this is image number
-            z = (row['x'] - P['focus_point']) * np.tan(angle)  # mean z at the x position
+        j += 1 # this is image number
+        z = (row['x'] - P['focus_point']) * np.tan(angle)  # mean z at the x position
 
-            # Make the label file
+        if background_path:
+            snippet, background_mean = snippets[i]
+            row = pd.DataFrame([{'filename': 'im{:06d}.png'.format(j), 'mean_z': z, 'norm': 1/(1e-8+background_mean)}])
+        else:
+            snippet = snippets[i]
             row = pd.DataFrame([{'filename' : 'im{:06d}.png'.format(j), 'mean_z' : z}])
-            df = pd.concat([df, row], ignore_index=True)
 
-            # Save image
-            imageio.imwrite(os.path.join(img_path, 'im{:06d}.png'.format(j)), snippets[i])
+        # Make the label file
+        df = pd.concat([df, row], ignore_index=True)
+
+        # Save image
+        imageio.imwrite(os.path.join(img_path, 'im{:06d}.png'.format(j)), snippet)
 
         i += 1
 
@@ -130,7 +137,10 @@ for image in tqdm.tqdm(movie.frames(), total=n_frames):
 if not P['in_pixel']: # save in the same unit as in the trajectory file
     df['mean_z'] *= pixel_size
 
-P['normalization'] = normalization
+if not background_path:
+    P['normalization'] = float(1./np.mean(intensities))
+else:
+    P['normalization'] = 1
 
 ## Save labels
 df.to_csv(label_path, index=False)

dataset/create_stack_dataset.py
@@ -35,7 +35,7 @@ if not os.path.exists(img_path):
     os.mkdir(img_path)
 
 ### Parameters
-parameters = [('step', 'Step (um)', -100),
+parameters = [('step', 'Step', -100), # in units of the trajectory files (pixel or um)
               ('pixel_size', 'Pixel size (um)', 5.),
               ('image_size', 'Image size (um)', 200)
               ]
@@ -66,6 +66,7 @@ folders.sort()
 
 ### Iterate folders
 j = 0
+intensities = []
 for i, subfolder in enumerate(folders):
     z = i*P['step']
     print('z = ', z, 'um')
@@ -90,6 +91,7 @@ for i, subfolder in enumerate(folders):
     for image in tqdm.tqdm(movie.frames(), total=n_frames):
         data_frame = data[data['frame'] == previous_position]
         snippets = extract_cells(image, data_frame, image_size, crop=True) # border cells are kept
+        intensities.extend([np.mean(snippet) for snippet in snippets])
 
         i = 0
         for _, row in data_frame.iterrows():
@@ -106,7 +108,7 @@ for i, subfolder in enumerate(folders):
 
         previous_position = movie.position
 
-P['normalization'] = normalization
+P['normalization'] = float(1./np.mean(intensities))
 
 ## Save labels
 df.to_csv(label_path, index=False)

dataset/create_trajectory_dataset.py
@@ -42,6 +42,7 @@ if not os.path.exists(img_path):
 
 ### Parameters
 parameters = [('angle', 'Angle (°)', 19.2), # signed
+              ('in_pixel', 'Trajectory in pixel', True),
               ('focus_point', 'Focus point (%)', 100), # x position where in focus
               ('pixel_size', 'Pixel size (um)', 5.),
               ('image_size', 'Image size (um)', 200),
@@ -57,6 +58,11 @@ P['image_size'] = (int(P['image_size']/pixel_size)//32)*32
 image_size = P['image_size']
 half_img_size = int(image_size/2)
 min_distance = P['min_distance'] # trajectories must span sufficient distance to be included
+
+### Put data in pixels
+if not P['in_pixel']:
+    data['x'] /= pixel_size
+    data['y'] /= pixel_size
 
 ### Calculate the interval between frames
 total_frames = data['frame'].nunique()
@@ -104,9 +110,11 @@ data = pd.concat(selected_segments)
 j = 0
 previous_position = 0
 rows = {}
+intensities = []
 for image in tqdm.tqdm(movie.frames(), total=n_frames):
     data_frame = data[data['frame'] == previous_position]
     snippets = extract_cells(image, data_frame, image_size, crop=True, borders=False) # remove border cells because they give away x and therefore z
+    intensities.extend([np.mean(snippet) for snippet in snippets])
 
     i = 0 # this is snippet number
     for row_index, row in data_frame.iterrows():
@@ -139,10 +147,13 @@ df = pd.DataFrame({'filename': filenames,
                    'mean_z': z,
                    'mask': mask})
 
+if not P['in_pixel']: # save in the same unit as in the trajectory file
+    df['mean_z'] *= pixel_size
+
 ## Save labels
 df.to_csv(label_path, index=False)
 
 ## Save parameters
-P['normalization'] = normalization
+P['normalization'] = float(1./np.mean(intensities))
 with open(parameter_path, 'w') as f:
     yaml.dump(P, f)

evaluation/evaluate_stack.py
@@ -15,7 +15,7 @@ import seaborn as sns
 matplotlib.use('TkAgg')
 from pylab import *
 import yaml
-from scipy.stats import pearsonr
+from scipy.stats import linregress
 
 pixel_size = 1.78
 
@@ -29,17 +29,20 @@ info_path = os.path.splitext(label_path)[0]+'_results.yaml'
 info = {}
 
 df = pd.read_csv(label_path)
+df = df[df['mean_z']>-550] # the recording at -600 is not good
 df = df.sort_values(by='mean_z')
 df['z_predict'] = df['z_predict']*pixel_size
 
 z_predict, mean_z = df['z_predict'].values, df['mean_z'].values
 MAE = np.mean(np.abs(z_predict-mean_z))
 print("Mean absolute error:", MAE)
-info['MAE'] = MAE
+info['MAE'] = float(MAE)
 
 ## Linear correlation
-correlation, _ = pearsonr(z_predict, mean_z)
-info['r'] = correlation
+result = linregress(z_predict, mean_z)
+slope = result.slope
+info['slope'] = float(slope)
+print("Regression slope:", slope)
 
 fig, (ax1, ax2) = subplots(2, 1, sharex=True)
 sns.histplot(df['z_predict'], kde=True, ax=ax1)
@@ -67,6 +70,9 @@ xlabel('Mean z (um)')
 ylabel('Estimated z (um)')
 subplot(212)
 std_estimate = df.groupby('mean_z')['z_predict'].std()
+precision = float(std_estimate.mean())
+info['precision'] = precision
+print("Precision:", precision)
 std_estimate.plot(kind='line', marker='o')
 xlabel('Mean z (um)')
 ylabel('Precision (um)')

evaluation/evaluate_tilted_slide.py
@@ -12,8 +12,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 from pylab import *
 import yaml
-from sklearn.feature_selection import mutual_info_regression
-from scipy.stats import pearsonr
+from scipy.stats import linregress
 
 #pixel_size = 1.78
 
@@ -33,20 +32,19 @@ z_predict, mean_z = df['z_predict'].values, df['mean_z'].values
 ## Error
 RMSE = ((z_predict-mean_z)**2).mean()**.5
 print("Error vs. mean_z:", RMSE)
-info["RMSE"] = RMSE
+info["RMSE"] = float(RMSE)
 
 MAE = np.mean(np.abs(z_predict-mean_z))
 print("Mean absolute error:", MAE)
-info['MAE'] = MAE
-
-## Calculate mutual information
-MI = mutual_info_regression(z_predict, mean_z, discrete_features=[False])
-print("Mutual information:", MI, "bits")
-info["MI"] = MI
+info['MAE'] = float(MAE)
 
 ## Linear correlation
-correlation, _ = pearsonr(z_predict, mean_z)
-info['r'] = correlation
+result = linregress(z_predict, mean_z)
+slope = result.slope
+info['slope'] = float(slope)
+info['r2'] = float(result.rvalue)**2
+print("Regression slope:", slope)
+print("Regression r2:", info['r2'])
 
 zmin, zmax = mean_z.min(), mean_z.max()
 
@@ -67,7 +65,7 @@ subplot(211)
 plot(mean_z, z_predict, '.k')
 plot([zmin, zmax], [zmin, zmax], 'b')
 plot(bin_centers, bin_means, "r")
-ylabel("Estimate of z (um)")
+ylabel("Estimate of z")
 
 subplot(413)
 plot(bin_centers, [np.std(z_predict[bin_indices == i]) for i in range(1, num_bins + 1)], "r", label='s.d.')
@@ -75,13 +73,13 @@ plot(bin_centers, [np.std(z_predict[bin_indices == i]) for i in range(1, num_bin
 plot(bin_centers, [np.mean((z_predict[bin_indices == i]-mean_z[bin_indices == i])**2)**.5 for i in range(1, num_bins + 1)], "k", label='error')
 plot(bin_centers, [np.mean((z_predict[bin_indices == i]-mean_z[bin_indices == i])) for i in range(1, num_bins + 1)], "b", label='bias')
 plot(bin_centers, 0*bin_centers, "--b")
-ylabel('Error (um)')
+ylabel('Error')
 #ylim(bottom=0)
 legend()
 
 subplot(414)
 plot(bin_centers, [np.sum([bin_indices == i]) for i in range(1, num_bins + 1)], "r")
-xlabel('Mean z (um)')
+xlabel('Mean z')
 ylabel('Density')
 ylim(bottom=0)
 

movie/cell_extraction.py
@@ -7,11 +7,11 @@ from scipy import stats
 
 __all__ = ['extract_cells']
 
-def extract_cells(image, data_frame, size, fill_value=0, crop=False, borders=True):
+def extract_cells(image, data_frame, size, fill_value=0, crop=False, background=None):
     '''
     Extract cell images from a big image with cell coordinates in pixel.
     * `size`: size of the output image
-    * `borders`: if `True`, keep cells without bounding box crossing the border
+    * `background`: if not None, returns the mean background too
     '''
     image_height, image_width = image.shape
 
@@ -52,6 +52,10 @@ def extract_cells(image, data_frame, size, fill_value=0, crop=False, borders=Tru
         dx, dy = int(dx), int(dy)
 
         snippet[dy:dy+y2-y1, dx:dx+x2-x1] = image[y1:y2, x1:x2]
-        snippets.append(snippet)
+        if background is not None:
+            background_mean = np.mean(background[y1:y2, x1:x2])
+            snippets.append((snippet, background_mean))
+        else:
+            snippets.append(snippet)
 
     return snippets